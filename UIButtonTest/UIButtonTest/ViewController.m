0b6074e6e95f89b021555e378fd78ea180a044ee
susu <28025920@qq.com>
2016-04-02 10:51:48 +0800

    项目完成

--- UIButtonTest/UIButtonTest/ViewController.m
+++ UIButtonTest/UIButtonTest/ViewController.m
@@ -9,19 +9,37 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutlet UIImageView *myImageView;
+@property(nonatomic)int x;
+@property(nonatomic)int y;
+@property(nonatomic)CGPoint* centerPoint;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //初始化点击按钮图片的x,y的移动值和放大值
+    self.x=10;
+    self.y=10;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)moveUP:(UIButton *)sender {
+    self.myImageView.center=CGPointMake(self.myImageView.center.x, self.myImageView.center.y-self.y);
+}
+- (IBAction)moveDown:(UIButton *)sender {
+    self.myImageView.center=CGPointMake(self.myImageView.center.x, self.myImageView.center.y+self.y);
+}
+- (IBAction)moveRight:(UIButton *)sender {
+    self.myImageView.center=CGPointMake(self.myImageView.center.x+self.x, self.myImageView.center.y);
+}
+- (IBAction)moveLeft:(id)sender {
+    self.myImageView.center=CGPointMake(self.myImageView.center.x-self.x, self.myImageView.center.y);
+}
+- (IBAction)imageViewEnlarge:(UIButton *)sender {
+    self.myImageView.bounds=CGRectMake(0, 0, self.myImageView.bounds.size.width+10, self.myImageView.bounds.size.height+10);
+}
+- (IBAction)imageViewShrink:(UIButton *)sender {
+    self.myImageView.bounds=CGRectMake(0, 0, self.myImageView.bounds.size.width-10, self.myImageView.bounds.size.height-10);
 }
 
 @end
